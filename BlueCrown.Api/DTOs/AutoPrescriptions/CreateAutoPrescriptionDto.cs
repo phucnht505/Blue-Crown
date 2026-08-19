13ac749278c456234b5b021ee24185e08275cb82
PHÚC NGUYỄN HOÀNG TẤN <2351010164phuc@ou.edu.vn>
2026-08-19 09:32:20 +0700

    feat: implement auto prescription API

---
+++ BlueCrown.Api/DTOs/AutoPrescriptions/CreateAutoPrescriptionDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlueCrown.Api.DTOs.AutoPrescriptions
+{
+    public class CreateAutoPrescriptionDto
+    {
+        public string DiseaseName { get; set; } = null!;
+        public Guid? RecommendedProductId { get; set; }
+        public string? DosageInstructions { get; set; }
+    }
+}
